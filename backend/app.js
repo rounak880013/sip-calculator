6048114fd0ea809942ad23bf16f75887edb63255
Rounak-alyve <rounak.raj@alyve.health>
2024-07-24 21:22:24 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
@@ -19,6 +20,14 @@ app.use(
 app.use('/api/admin',adminRoutes);
 
 let port = process.env.PORT || 3000;
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
 app.listen(port, function () {
   console.log(`server is listening on port ${port}`);
 });
