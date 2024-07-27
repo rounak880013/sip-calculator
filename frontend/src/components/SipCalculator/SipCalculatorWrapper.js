7fe58e1d35bef1843fcda10054ba51be841529a6
Rounak-alyve <rounak.raj@alyve.health>
2024-07-27 23:43:58 +0530

    hamlet async

--- frontend/src/components/SipCalculator/SipCalculatorWrapper.js
+++ frontend/src/components/SipCalculator/SipCalculatorWrapper.js
@@ -7,18 +7,18 @@ import { data } from './data'; // Import the metadata
 
 export const SipCalculatorWrapper = ({ sipType, stepUpPercentage }) => {
   const { title, description, keywords } = data[sipType] || data["sip"];
-  useEffect(() => {
-    // Destructure the title from the data object based on sipType
-    const { title } = data[sipType] || data["sip"];
+  // useEffect(() => {
+  //   // Destructure the title from the data object based on sipType
+  //   const { title } = data[sipType] || data["sip"];
     
-    // Set the document title
-    document.title = title;
-  }, [sipType]);
+  //   // Set the document title
+  //   document.title = title;
+  // }, [sipType]);
 
   return (
     <div>
       <Helmet>
-        {/* <title>{title}</title> */}
+        <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
         <link rel="canonical" href={`https://stepupsipcalculator.co.in/${sipType === 'step-up' ? 'stepup-sip-calculator' : 'sip-calculator'}`} />
